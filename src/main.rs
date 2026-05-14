3b3848fd723e1879ce89c51d5888705f6444e395
James Gardner <jamesgardner@live.com.au>
2026-05-14 14:49:59 +1000

    Model class demo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use http_server::web_server::{
-    Body, ErrorRoute, Parameters, Request, RequestMethod::Get, RequestMethod::Post, Response,
-    Route, StatusCode, WebServer,
+    Body, ErrorRoute, Parameters, Request,
+    RequestMethod::{Get, Post},
+    Response, Route, StatusCode, WebServer,
 };
 
-fn route_form_submission(
+struct Greeting {
+    say: String,
+    to: String,
+}
+
+impl TryFrom<&Parameters> for Greeting {
+    type Error = StatusCode;
+    fn try_from(params: &Parameters) -> Result<Self, Self::Error> {
+        match (params.get("say"), params.get("to")) {
+            (Some(say), Some(to)) => Ok(Greeting {
+                say: say.clone(),
+                to: to.clone(),
+            }),
+            _ => Err(StatusCode::BadRequest),
+        }
+    }
+}
+
+fn route_greeting_form_submission(
     request: &Request,
     _path_params: Parameters,
 ) -> Result<Response, StatusCode> {
     match &request.body {
-        Body::FormData(params) => Ok(Response::ok(format!("{:?}", params))),
+        Body::FormData(params) => {
+            let greeting = Greeting::try_from(params)?;
+            Ok(Response::ok(format!("I will say {} to {}", greeting.say, greeting.to)))
+        }
         _ => Err(StatusCode::UnsupportedMediaType),
     }
 }
@@ -54,8 +76,8 @@ fn main() {
         Box::new([
             Route::file(Get, "/", "html/index.html"),
             Route::file(Get, "/index.html", "html/index.html"),
-            Route::file(Get, "/form.html", "html/form.html"),
-            Route::func(Post, "/form_submission", route_form_submission),
+            Route::file(Get, "/greeting_form", "html/form.html"),
+            Route::func(Post, "/greeting_form", route_greeting_form_submission),
             Route::func(Get, "/query_params", route_query_params),
             Route::func(Get, "/user/me", route_get_me),
             Route::func(Get, "/user/{id}", route_get_user),
